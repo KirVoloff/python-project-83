bb2b0b6450e263e32cb99a55ff7d93604e0d5cea
Vol'nov Kirill <volnov-93@mail.ru>
2023-07-25 21:13:47 +0300

    database.sql

--- database.sql
+++ database.sql
@@ -1,15 +1,19 @@
-DROP TABLE IF NOT EXISTS urls (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name varchar(255) UNIQUE,
-    created_at timestamp NOT NULL
+DROP TABLE IF EXISTS url_checks;
+
+DROP TABLE IF EXISTS urls;
+
+CREATE TABLE IF NOT EXISTS urls (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP NOT NULL
 );
 
-DROP TABLE IF NOT EXISTS url_checks (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id bigint REFERENCES urls (id),
-    status_code varchar(3),
-    h1 varchar(255),
-    title varchar(255),
-    description text,
-    created_at timestamp NOT NULL
+CREATE TABLE IF NOT EXISTS url_checks (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id BIGINT REFERENCES urls (id),
+    status_code VARCHAR(3),
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP NOT NULL
 );
