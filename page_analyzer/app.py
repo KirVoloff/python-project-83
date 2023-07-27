89082fe2ec73abbca413d4f8507ce87dec1b1b7e
Vol'nov Kirill <volnov-93@mail.ru>
2023-07-27 08:35:32 +0300

    page_analyzer/utils.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,6 @@
 import page_analyzer.db as db
 from page_analyzer.config import Config
 from page_analyzer.services import get_response, parse_page, get_correct_url
-from page_analyzer.utils import handle_invalid_url,\
-    handle_existing_url,\
-    handle_error,\
-    handle_success
 
 import logging
 
@@ -105,3 +101,25 @@ def url_check(id):
         logging.error(err)
         flash('Произошла ошибка при проверке', 'alert-danger')
         return redirect(url_for('url_get', id=id))
+
+
+def handle_invalid_url(url):
+    flash('Некорректный URL', 'alert-danger')
+    messages = get_flashed_messages(with_categories=True)
+    return render_template('index.html', url=url, messages=messages), 422
+
+
+def handle_existing_url(url):
+    flash('Страница уже существует', 'alert-info')
+    return redirect(url_for('url_get', id=db.find_url(url).id))
+
+
+def handle_error(url):
+    flash('Произошла ошибка', 'alert-danger')
+    messages = get_flashed_messages(with_categories=True)
+    return render_template('index.html', url=url, messages=messages), 500
+
+
+def handle_success(result):
+    flash('Страница успешно добавлена', 'alert-success')
+    return redirect(url_for('url_get', id=result))
